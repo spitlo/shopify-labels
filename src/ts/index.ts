2ca0017f58baac6690dae80bb0e6bb74f3e58e7e
Jakob Bergman <jakob@aino.se>
2020-06-29 10:25:38 +0200

    Generate new labels automatically on change

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -6,17 +6,24 @@ export function init(): void {
   const labelForm: any = document.getElementById('labelForm')
   labelForm.onsubmit = handleFiles
   labelForm.onchange = () => {
-    document.getElementById('submit-button').removeAttribute('disabled')
+    const { elements } = labelForm
+    let { file } = elements
+    file = file.files[0]
+    if (file) {
+      handleFiles()
+    }
   }
   // Temp, remove when/if posthtml-expressions is working
   labelForm.elements[1].value = DEFAULT_COLUMNS
   labelForm.elements[2].value = DEFAULT_ROWS
-  const filePicker = document.getElementById('file-picker')
-  filePicker.onchange = handleFiles
+  // const filePicker = document.getElementById('file-picker')
+  // filePicker.onchange = handleFiles
 }
 
-export function handleFiles(event): void {
-  event.preventDefault()
+export function handleFiles(event?): void {
+  if (event) {
+    event.preventDefault()
+  }
   const labelForm: any = document.getElementById('labelForm')
   const { elements } = labelForm
   let { columns, rows, file } = elements
@@ -29,9 +36,9 @@ export function handleFiles(event): void {
   }
 
   // Change button text to make more sense, and disable it until changes occur
-  const submitButton = document.getElementById('submit-button')
-  submitButton.textContent = 'Re-generate labels!'
-  submitButton.setAttribute('disabled', 'disabled')
+  // const submitButton = document.getElementById('submit-button')
+  // submitButton.textContent = 'Re-generate labels!'
+  // submitButton.setAttribute('disabled', 'disabled')
 
   if (window.FileReader) {
     Papa.parse(file, {
